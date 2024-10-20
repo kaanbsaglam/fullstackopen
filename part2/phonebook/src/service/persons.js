b203c906018fd3195423d77f414ad34ca42cbcb0
kaanbsaglam <kaanberk2009@hotmail.com>
2024-10-20 20:56:35 +0300

    change base url to match the server made in part3

--- part2/phonebook/src/service/persons.js
+++ part2/phonebook/src/service/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons' // or add proxy to the vite.config.js file
 
 const getAll = () => {
     const request = axios.get(baseUrl)
