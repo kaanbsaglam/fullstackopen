fda663318de2ccbd4379076a9cfeeaa26fa5d452
kaanbsaglam <kaanberk2009@hotmail.com>
2024-11-13 22:19:10 +0300

    complete 4.11 up to 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -13,19 +13,47 @@ blogsRouter.get('/', async (request, response) => {
 })
   
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const {title, url, author, likes} = request.body
+
+  if(!title || !url){
+    return response.status(400).json({
+      error: "missing url and/or title"
+    })
+  }
+  const blog = new Blog({
+    title:title,
+    url:url,
+    author: author,
+    likes: likes || 0
+  })
 
   const savedBlog = await blog.save()
-
   response.status(201).json(savedBlog)
-
-  
-/*     blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      }) */
 })
+
+blogsRouter.delete('/:id', async (request,response) => {
+  const blogToDelete = await Blog.findByIdAndDelete(request.params.id)
+
+  response.status(204).end()
+
+})
+
+blogsRouter.put('/:id', async (request,response) => {
+  const {title,url,author,likes} = request.body
+  const requestedBlog = {
+    title: title,
+    url: url,
+    author: author,
+    likes: likes || 0
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, requestedBlog, {new:true, runValidators:true, context:'query'})
+
+  response.json(updatedBlog)
+
+})
+
+
 
 module.exports = blogsRouter
   
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -24,7 +24,7 @@ test('blogs are returned as json, and the correct amount', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, 1)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 test('blogs unique identifier is called id, and not _id', async () => {
@@ -33,7 +33,7 @@ test('blogs unique identifier is called id, and not _id', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/)
     
-    response.body.forEach(blog => {
+    response.body.forEach(blog => { 
         assert(blog.id)
         assert.strictEqual(blog._id, undefined)
     })
@@ -66,6 +66,107 @@ test('a valid blog can be added', async () => {
     assert(blogTitles.includes(newBlog.title))
 
 })
+
+test('a blog without a like property is added and like is set to 0', async () => {
+    const newBlog = {
+        title:"newBlog123",
+        author: "newGuy",
+        url: "suchanewsite.com",    
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtTheEnd = await helper.blogsInDb()
+
+    const addedBlog = blogsAtTheEnd.find(blog => blog.title === newBlog.title);
+
+    assert.strictEqual(addedBlog.likes,0)
+    
+})
+
+test("a blog with url or title missing is not added", async () => {
+    const missingUrl = {
+        title:"newBlog123",
+        author: "newGuy",
+        likes: 1
+    }
+    const missingTitle = {
+        url: "suchanewsite.com",    
+        author: "newGuy",
+        likes: 1
+    }
+    
+    await api
+        .post('/api/blogs')
+        .send(missingUrl)
+        .expect(400)
+
+
+    await api
+        .post('/api/blogs')
+        .send(missingTitle)
+        .expect(400)
+
+    const blogsAtTheEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtTheEnd.length, helper.initialBlogs.length)
+})
+
+test("deleting a single blog", async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+
+    const firstBlogsId = response.body[0].id
+
+    await api
+        .delete(`/api/blogs/${firstBlogsId}`)
+        .expect(204)
+    
+    const blogsAtTheEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtTheEnd.length,helper.initialBlogs.length-1)
+
+    
+
+    const deleted = await Blog.findById(firstBlogsId)
+
+    assert(!deleted)
+
+})
+
+test("updating a single blog", async () => {
+    const response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+    const firstBlog =  response.body[0]
+
+    const firstBlogsId = response.body[0].id
+
+    const updatedBlogJson = {
+        title: firstBlog.title,
+        author: firstBlog.author,
+        url: firstBlog.url,
+        likes: 777
+    }
+
+    const updatedResponse = await api
+        .put(`/api/blogs/${firstBlogsId}`)
+        .send(updatedBlogJson)
+        .expect(200)
+
+    const updatedBlog = await Blog.findById(firstBlogsId)
+
+    assert.strictEqual(updatedBlog.likes,updatedBlogJson.likes)
+
+})
+
+
+
 
 
 after(async () => {
