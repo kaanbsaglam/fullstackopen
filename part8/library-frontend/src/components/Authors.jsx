db56715794264f924224448d9712267a7dcf8613
kaanbsaglam <kaanberk2009@hotmail.com>
2025-03-09 13:11:26 +0300

    complete part 8.12

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,22 +1,37 @@
 import { useQuery } from '@apollo/client'
 import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
-import EditBirthyearForm from './EditBirthyearForm'
-
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
 
 const Authors = () => {
 
   const result = useQuery(ALL_AUTHORS)
-  console.log(result)
-
-
+  const [selectedAuthor, setSelectedAuthor] = useState("")
+  const [born, setBorn] = useState("")
+  const [ changeBirthyear ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
 
   if(result.loading) {
     return <div>loading...</div>
   }
 
+
   if(result.error) {  
     console.log(result.error)
     return <div>Error</div>
+  }
+
+  const submit = (event) => {
+    event.preventDefault()
+    if(!selectedAuthor || !born) {
+      console.error("Author and birthyear must be selected")
+      return
+    }
+    changeBirthyear({ variables: { name:selectedAuthor, setBornTo: Number(born) } })
+
+    setSelectedAuthor('')
+    setBorn('')
   }
 
   const authors = result.data.allAuthors
@@ -41,10 +56,38 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
-      <h2>Set birthyear</h2>
-      <EditBirthyearForm /> 
+      <h2>Set Birth Year</h2>
+      <form onSubmit={submit}>
+        <div>
+          <label>Author:</label>
+          <select
+            value={selectedAuthor}
+            onChange={({ target }) => setSelectedAuthor(target.value)}
+          >
+            <option value="" disabled>Select an author</option>
+            {result.data.allAuthors.map((author) => (
+              <option key={author.name} value={author.name}>
+                {author.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label>Birth Year:</label>
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+
+        <button type="submit">Update Author</button>
+      </form>
     </div>
   )
 }
+
+
 
 export default Authors
